248bacdd706c946753483f755b79a268eb1ad865
Robson Silva <robsantossilva@gmail.com>
2022-07-25 21:45:54 -0300

    ci: :green_heart: check if quality gate is working on sonar cloud

--- go/sum.go
+++ go/sum.go
@@ -12,14 +12,14 @@ func sum(a, b int) int {
 	return result
 }
 
-func sub(a int, b int) int {
-	return a - b
-}
+// func sub(a int, b int) int {
+// 	return a - b
+// }
 
-func times(a int, b int) int {
-	return a * b
-}
+// func times(a int, b int) int {
+// 	return a * b
+// }
 
-func sumX(a int, b int) int {
-	return a + b + a
-}
+// func sumX(a int, b int) int {
+// 	return a + b + a
+// }
